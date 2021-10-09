038394b8d205a31a8b8b71c6ddf8b878f69f53bc
enonnemacher <enonnemacher@gmail.com>
2021-10-09 12:09:50 -0300

    Ajustes ao instanciar as classes ator e diretor

--- Aula02/exercicios-aula-2/src/main/java/main/Aplicacao.java
+++ Aula02/exercicios-aula-2/src/main/java/main/Aplicacao.java
@@ -3,20 +3,20 @@ package main;
 public class Aplicacao {
     public static void main(String[] args) {
 
-        Diretor diretor = new Diretor("Quentin Tarantino", 60, 30, Genero.MASCULINO);
-        Ator ator = new Ator("Daniel Craig", 53, 2, Genero.MASCULINO);
+        Diretor quentinTarantino = new Diretor("Quentin Tarantino", 60, 30, Genero.MASCULINO);
+        Ator danielCraig = new Ator("Daniel Craig", 53, 2, Genero.MASCULINO);
 
-        Filme django = new Filme("Django", "Filme ação", 120, 2016, 5, diretor);
-        Filme pulpFiction = new Filme("Pulp fiction", "Filme ação", 130, 1997, 5, diretor);
+        Filme django = new Filme("Django", "Filme ação", 120, 2016, 5, quentinTarantino);
+        Filme pulpFiction = new Filme("Pulp fiction", "Filme ação", 130, 1997, 5, quentinTarantino);
 
         django.reproduzir();
         System.out.println();
         pulpFiction.reproduzir();
         System.out.println();
         System.out.println("Dados do diretor:");
-        diretor.retornaDados();
+        quentinTarantino.retornaDados();
         System.out.println();
         System.out.println("Dados do ator:");
-        ator.retornaDados();
+        danielCraig.retornaDados();
     }
 }
